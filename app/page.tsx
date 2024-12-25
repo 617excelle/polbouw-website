b45cc4267b8a003e37724c622f2460ff100ccb82
Jordan Bentkowski <jordan.bentkowski@student.hogent.be>
2024-12-25 18:22:21 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -44,29 +44,15 @@ export default function Component() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <section className="min-h-screen pt-16 md:pt-20 bg-gradient-to-br from-blue-500 via-blue-600 to-indigo-900 relative overflow-hidden">
-        <div className="container mx-auto px-4 h-full flex items-center">
-          <div className="grid md:grid-cols-2 gap-8 md:gap-12 items-center">
-            <div className="text-white space-y-6 md:space-y-8">
-              <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
-                Snelle toegang tot experts en onderaannemers, zonder lange
-                verplichtingen.
-              </h1>
-              <Button className="w-full sm:w-auto bg-white hover:bg-white/90 text-blue-500 font-bold rounded-md px-6 py-4 sm:px-8 sm:py-6 text-base sm:text-lg transition-colors duration-200">
-                Waar wacht je nog op
-              </Button>
-            </div>
-            <div className="mt-8 md:mt-0">
-              {/* <Image
-                className="rounded-md w-full h-auto object-cover mb-8 "
-                src="/images/worker.jpg"
-                alt="An example image"
-                width={500}
-                height={300}
-                layout="responsive"
-              /> */}
-            </div>
-          </div>
+      <section className="h-[80vh]  flex items-center  bg-gradient-to-br from-blue-500 via-blue-600 to-indigo-900 ">
+        <div className="container mx-auto text-white space-y-6 flex flex-col">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-tight">
+            Snelle toegang tot experts en onderaannemers, zonder lange
+            verplichtingen.
+          </h1>
+          <Button className="w-72  bg-white hover:bg-white/90 text-blue-500 font-bold rounded-md px-6 py-4 sm:px-8 sm:py-6 text-base sm:text-lg transition-colors duration-200">
+            Waar wacht je nog op
+          </Button>
         </div>
       </section>
       <section className="bg-gradient-to-tr from-blue-500 via-blue-600 to-indigo-900">
